0f349e1697b5fcad5f298dfdf893f4b4235de1a2
harrymontenegro <harryjm018@hotmail.com>
2022-11-14 22:25:33 -0500

    Refactor test OdontologoTest.java

---
+++ src/test/java/com/app/clinicamedica/OdontologoTest.java
@@ -0,0 +1,28 @@
+package com.app.clinicamedica;
+
+
+import com.app.clinicamedica.models.Odontologo;
+import com.app.clinicamedica.service.OdontologoService;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class OdontologoTest {
+
+    @Test
+    void instanciarOdontologoTest(){
+        Odontologo odontologo = new Odontologo();
+        assertNotNull(odontologo);
+    }
+
+    @Test
+    void listarOdontologos(){
+        OdontologoService service = new OdontologoService();
+        List<Odontologo> lista = service.listarTodosOdontologos();
+        assertTrue(lista.size() > 0);
+    }
+}
